68b5beda641921dfd94528a7543e30188bb04ed9
james602152002@hotmail.com <b22812834>
2020-12-29 16:21:28 +0800

    Update

--- translate.py
+++ translate.py
@@ -2,11 +2,16 @@
 from googletrans import Translator
 trans = Translator()
 name = input("Plz input name:")
-enTrans = trans.translate(name, src = "zh-cn", dest = "en")
-print("en:\n" + enTrans.text)
-twTrans = trans.translate(name, src = "zh-cn", dest = "zh-tw")
-print("tw:\n" + twTrans.text)
-jpTrans = trans.translate(name, src = "zh-cn", dest = "ja")
-print("jp:\n" + jpTrans.text)
-krTrans = trans.translate(name, src = "zh-cn", dest = "ko")
-print("ko:\n" + krTrans.text)
+lanArr = ["en", "zh-tw", "ja", "ko"]
+
+for lan in lanArr:
+ lanTrans = trans.translate(name, src = "zh-cn", dest = lan)
+ print(lan + ":\n" + lanTrans.text)
+#enTrans = trans.translate(name, src = "zh-cn", dest = "en")
+#print("en:\n" + enTrans.text)
+#twTrans = trans.translate(name, src = "zh-cn", dest = "zh-tw")
+#print("tw:\n" + twTrans.text)
+#jpTrans = trans.translate(name, src = "zh-cn", dest = "ja")
+#print("jp:\n" + jpTrans.text)
+#krTrans = trans.translate(name, src = "zh-cn", dest = "ko")
+#print("ko:\n" + krTrans.text)
